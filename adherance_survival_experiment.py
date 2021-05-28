77b52780d681f8ff498f4ff2332b5492bcc7f2da
eshanking <33136282+eshanking@users.noreply.github.com>
2021-05-28 17:15:48 -0400

    Add files via upload

---
+++ adherance_survival_experiment.py
@@ -0,0 +1,33 @@
+from experiment_class_raw import Experiment
+import numpy as np
+
+init_counts = np.zeros(16)
+init_counts[0] = 10**5
+
+options = {'doubling_time':1.5,
+           'death_rate':0.016,
+           'mut_rate':10**-9,
+           'carrying_cap':True,
+           'max_cells':10**11,
+           'n_timestep':1000,
+           'init_counts':init_counts,
+           'k_abs':0.95,
+           'k_elim':0.00839,
+           'max_dose':1100,
+           'dose_schedule':24,
+           'pad_right':True,
+           'timestep_scale':2,
+           'plot':False}
+
+p = np.array([0,0.2,0.4,0.6,0.8])
+n_sims = 100
+
+experiment_type = 'drug-regimen'
+
+e = Experiment(experiment_type=experiment_type,
+               n_sims=n_sims,
+               prob_drops=p,
+               population_options = options,
+               debug=False)
+
+e.run_experiment()
