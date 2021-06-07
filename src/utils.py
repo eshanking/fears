b6a03d833c78b54795e07ba1477aa1185a722c00
King <kinge2@lri-108705.ccf.org>
2021-06-07 13:17:43 -0400

    added utils module

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+def make_datapath_absolute(filename):
+    r = str(get_project_root())    
+    p = r + os.sep + 'data' + os.sep + filename
+    return p
