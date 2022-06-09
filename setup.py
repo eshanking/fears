bb0b27d14724903bac37180d69219367695dd483
King <kinge2@lri-108705.cc.ad.cchs.net>
2022-06-09 14:18:33 -0400

    redid file structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fears', 
+    version='0.1.0', 
+    author = 'Eshan King', 
+    author_email = '',
+    # packages=['autorate', "autorate.test", "autorate.test.data"], 
+    # pacakges=['fears','fears.data','fears.utils'],
+    packages=find_packages(include=['fears','fears.population','fears.data','fears.utils','fears.utils.*']),
+    # packages=find_packages(where="fears"),
+    install_requires = [
+      "pandas",
+      "pytest",
+      "scipy",
+      "matplotlib",
+      "numpy",
+      "importlib_resources"
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']}
+)
